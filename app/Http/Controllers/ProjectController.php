852a86a7e1fe2a19bbabecfe45ba33bbb3ee2db6
Carl Scicluna <carl.m.scicluna@gmail.com>
2022-02-24 17:44:41 +0100

    passed tests

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
+        auth()->user()->projects()->sync([
+            $request->project_id, [
+                'start_date' => $request->start_date
+            ]
+        ]);
 
         return 'Success';
     }
